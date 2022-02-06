88adfda019aacafa42645b16e7fb009e0e57f583
soonseok2 <51370905+soonseok2@users.noreply.github.com>
2022-02-06 23:01:30 +0900

    Create 1874_스택수열_실버3.java

---
+++ soonseok/1874_스택수열_실버3.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int N = Integer.parseInt(br.readLine());
+
+		Stack<Integer> stack = new Stack<>();
+		Queue<Integer> queue = new LinkedList<>();
+
+		for (int i = 0; i < N; i++) {
+			queue.add(Integer.parseInt(br.readLine()));
+		}
+
+		int num = 1;
+
+		while (!queue.isEmpty()) { // out when queue is over
+
+			int target = queue.poll();
+
+			if (target >= num) {
+				for (int i = num; i <= target; i++) {
+					stack.push(i);
+					sb.append("+").append("\n");
+				}
+				num = target + 1; // 기존 target의 다음 값으로 간다.
+
+				stack.pop();
+				sb.append("-").append("\n");
+			} else {
+				if (stack.peek() == target) {
+					stack.pop();
+					sb.append("-").append("\n");
+				} else {
+					System.out.print("NO");
+					return;
+				}
+			}
+		} // end of while
+
+		System.out.print(sb);
+
+	} // end of main
+} // end of class
