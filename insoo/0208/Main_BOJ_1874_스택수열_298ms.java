10607c0d678608a83550c2b614ef9e8b6efa40e6
INSOO BAE <qospwmf@gmail.com>
2022-02-07 23:20:40 +0900

    Create Main_BOJ_1874_스택수열_298ms.java

---
+++ insoo/0208/Main_BOJ_1874_스택수열_298ms.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+    static int[] stack;
+    static int pos = 0;
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(bf.readLine());
+        int[] arr = new int[n];
+        stack = new int[n];
+        for (int i = 0; i < n; i++) {
+            int a = Integer.parseInt(bf.readLine());
+            arr[i] = a;
+        }
+        int k = 0;
+        for (int i = 1; k != n; ) {
+            if (arr[k] >= i) {
+                push(i++);
+                sb.append('+').append('\n');
+            } else if (arr[k] == top()) {
+                pop();
+                k++;
+                sb.append('-').append('\n');
+            } else {
+                System.out.println("NO");
+                return;
+            } // end of if-else
+        } // end of for
+
+        System.out.println(sb);
+        bf.close();
+    }
+
+    static void push(int x) {
+        stack[pos++] = x;
+    }
+
+    static void pop() {
+        pos--;
+    }
+
+    static int top() {
+        return stack[pos - 1];
+    }
+}
