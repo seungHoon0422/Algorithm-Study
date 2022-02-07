ca8bad939b8e973faa7273e9712912b126780050
Baek Seung Hoon <71703518+seonghoon96@users.noreply.github.com>
2022-02-07 15:36:20 +0900

    Update and rename BOJ_1543_문서검색_S4_백승훈_84ms.java to Main_1543_문서검색_S4_백승훈_84ms.java

--- seunghoon/0208/Main_1543_문서검색_S4_백승훈_84ms.java
+++ seunghoon/0208/Main_1543_문서검색_S4_백승훈_84ms.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class BOJ_1543_문서검색_S4_백승훈_84ms {
+public class Main_1543_문서검색_S4_백승훈_84ms {
 
 	static StringBuilder sb = new StringBuilder();
 	public static void main(String[] args) throws IOException {
