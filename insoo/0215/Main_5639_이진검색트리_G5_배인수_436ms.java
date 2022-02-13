5952b949062146f0a59e6f8b3df877a951550b7a
INSOO BAE <qospwmf@gmail.com>
2022-02-13 18:49:31 +0900

    Update Main_5639_이진검색트리_G5_배인수_436ms.java

--- insoo/0215/Main_5639_이진검색트리_G5_배인수_436ms.java
+++ insoo/0215/Main_5639_이진검색트리_G5_배인수_436ms.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 
-public class Main {
+public class Main_5639_이진검색트리_G5_배인수_436ms {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
